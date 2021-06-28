43cd65f988fb8534b0b83252b0063f213cc6e2e6
Pascal Pater <pascal.pater@gmail.com>
2021-06-28 12:39:44 +0200

    add quick buttons to test functions

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,39 @@
 import React from "react";
 import "./App.css";
 
-export default () => <div>Home!</div>;
+const Home = (props) => {
+
+  return (
+    <>
+      <div>Home!</div>
+      <button
+        onClick={() =>
+          props.addDentist(
+            "Toos",
+            "Trekker",
+            "06-12345678",
+            "toos@tandartspraktijkbvt.nl"
+          )
+        }
+      >
+        Add dentist
+      </button>
+      <button
+        onClick={() =>
+          props.addPatient(
+            "Piet",
+            "Auw",
+            "06-12345679",
+            "piet@wincacademy.nl",
+            1985
+          )
+        }
+      >
+        Add patient
+      </button>
+    </>
+  );
+
+}
+
+export default Home;
